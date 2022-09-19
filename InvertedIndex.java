c3c8855e54cf79b13a4115d92432e4b036538d36
gontar <a.k.gontar02@mail.ru>
2022-09-19 18:41:40 +0800

    bin

---
+++ InvertedIndex.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class InvertedIndex {
+    private  List<String> documents;
+    private Map<String, LinkedList<Integer>> index;
+
+    public InvertedIndex(){
+        documents = ;
+        index = new HashMap<>();
+
+    }
+    //пустой конструктор, присвоить докуменам или LinkedList, какой то другой, а индексу hashmap пустые коллекции
+    public void indexDocument(String path){
+
+    }
+
+}
